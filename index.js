99af64528c52afaacbfe8e89ab4a2ddbf38611fb
ramki45 <89958455+ramki45@users.noreply.github.com>
2022-01-15 15:46:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const mongodb = require("mongodb");
 const mongoClient = mongodb.MongoClient;
-const URL = "mongodb+srv://ramki45:pandian5!@cluster0.vna1k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const URL = "mongodb+srv://ramki45:pandian5!@cluster0.vna1k.mongodb.net/?retryWrites=true&w=majority";
 app.use(express.json());
 app.use(cors({
     origin: "*"
